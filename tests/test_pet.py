1a1072f7df4d62c58d1ee346c54f8bc5ee4af5ea
RAMISK <rkomiakov@mail.ru>
2025-12-02 14:02:26 +0300

    Внес исправления: - @allure.title - убрал лишную схему PET_SCHEMA_1

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -2,7 +2,6 @@ import allure
 import jsonschema
 import requests
 from .schemas.pet_schema import PET_SCHEMA
-from .schemas.pet_schema import PET_SCHEMA_1
 
 BASE_URL = "http://5.181.109.28:9090/api/v3"
 
@@ -43,7 +42,7 @@ class TestPet:
         with allure.step("Проверка текстового содержимого ответа"):
             assert response.text == "Pet deleted", "Текст ошибки не совпал с ожидаемым"
 
-#Создаю еще один пулл реквест, предыдущий удалился при закрытии PR#
+#Создаю еще один пулл реквест, предыдущий удалился при закрытии PR
 
     @allure.title("Добавление нового питомца")
     def test_add_pet(self):
@@ -66,9 +65,9 @@ class TestPet:
             assert response.json()['name'] == payload['name'], "имя питомца не совпадает с ожидаемым"
             assert response.json()['status'] == payload['status'], "статус питомца не совпадает с ожидаемым"
 
-    @allure.title("Добавление нового питомца")
-    def test_add_pet_1(self):
-        with allure.step("Подготовка данных для создания питомца"):
+    @allure.title("Добавление нового питомца c полными данными")
+    def test_add_pet_full_data(self):
+        with allure.step("Подготовка данных для создания питомца c полными данными"):
             payload = {
 
                 "id": 10,
@@ -80,16 +79,16 @@ class TestPet:
                 "photoUrls": ["string"],
                 "tags": [{"id": 0, "name": "string"}],
                 "status": "available"
-        }
+                }
 
-            with allure.step("Отправка запроса на создание питомца"):
+            with allure.step("Отправка запроса на создание питомца c полными данными"):
                 response = requests.post(url=f"{BASE_URL}/pet", json=payload)
 
             with allure.step("Проверка статуса ответа и валидация JSON-схемы"):
                 assert response.status_code == 200
-                jsonschema.validate(response.json(), PET_SCHEMA_1)
+                jsonschema.validate(response.json(), PET_SCHEMA)
 
-            with allure.step("Проверка параметров питомца в ответе"):
+            with allure.step("Проверка параметров питомца c полными данными в ответе"):
                 assert response.json()['id'] == payload['id'], "id питомца не совпадает с ожидаемым"
                 assert response.json()['name'] == payload['name'], "имя питомца не совпадает с ожидаемым"
                 assert response.json()['photoUrls'] == payload['photoUrls'], "фото питомца не совпадает с ожидаемым"
